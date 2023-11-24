edbff6b829ca009503db488b49c5eb55f45f20c6
Demolish2002 <gulyas.zsolt2002@gmail.com>
2023-11-24 15:17:06 +0100

    create backend and frontend communication through http requests

--- frontEnd/src/app/component/form/form.component.ts
+++ frontEnd/src/app/component/form/form.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
+import {FormDataModel} from "../../model/form-data.model";
+import {FormService} from "../../service/form.service";
 
 @Component({
   selector: 'app-form',
@@ -9,15 +11,22 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class FormComponent {
   basicForm: FormGroup
 
-  constructor() {
+  constructor(private formService: FormService) {
     this.basicForm = new FormGroup({
-        'name': new FormControl(''),
-        'number': new FormControl(0),
-      })
+      'name': new FormControl(''),
+      'number': new FormControl(0),
+    })
   }
 
-  handleSubmit(){
-
+  handleSubmit() {
+    const data: FormDataModel = this.basicForm.value;
+    this.formService.sendFormData(data).subscribe(
+      {
+        next: (value) => console.log("Form has been successfully saved"),
+        error:(err)=> console.log(err),
+        complete:()=>console.log("Completion branch has run")
+      }
+    )
   }
 
 }
